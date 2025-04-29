37ca23424a91ceee26f2584253c8a3f6fec591c5
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-04-29 02:38:53 -0300

    refactor: remove standart ui app name

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -78,15 +78,6 @@ export function AppSidebar(props) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link href="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Command className="size-4" />
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">Acme Inc</span>
-                  <span className="truncate text-xs">Enterprise</span>
-                </div>
-              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
