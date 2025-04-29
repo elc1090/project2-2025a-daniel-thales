cc24e051fc16a4d4b7c910e36d13623fbfb9d061
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-04-29 02:39:25 -0300

    feat: show home charts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,27 @@
 "use client"
 
+import { CaloriesPerMonthChart } from "@/components/CaloriesPerMonthChart"
+import { ExercisesPerMonthChart } from "@/components/ExercisesPerMonthChat"
+import { Chart } from "@/components/HoursPerMonthChar"
+import { TrainingProfileRadarChart } from "@/components/TrainingProfileRadarChart"
 
 export const iframeHeight = "800px"
-
 export const description = "A sidebar with a header and a search form."
 
 export default function Page() {
-
   return (
-    <div>
-      Início
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Início</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <Chart />
+        <ExercisesPerMonthChart />
+        
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-5">
+      <CaloriesPerMonthChart />
+        <TrainingProfileRadarChart />
+      </div>
     </div>
-  );
+  )
 }
